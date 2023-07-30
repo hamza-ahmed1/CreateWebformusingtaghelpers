db3fa602de732183a71742a7962bb2a7cc59545d
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-07-30 10:24:18 -0700

    Initial commit

---
+++ CreateWebformusingtaghelpers/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace CreateWebformusingtaghelpers.Models
+{
+    public class Student
+    {
+        public int Rollnum { get; set; }
+        public string? Name { get; set; }
+        public int    Age { get; set; }
+
+        public string? Email { get; set; }     
+    }
+}
