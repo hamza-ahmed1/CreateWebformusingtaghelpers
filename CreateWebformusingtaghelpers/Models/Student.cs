07e67693574ba675f3af5a204d73530664fa6295
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-07-31 02:02:59 -0700

    changes

--- CreateWebformusingtaghelpers/Models/Student.cs
+++ CreateWebformusingtaghelpers/Models/Student.cs
@@ -5,7 +5,15 @@
         public int Rollnum { get; set; }
         public string? Name { get; set; }
         public int    Age { get; set; }
+        public Gender Gender { get; set; }
+        public string? Email { get; set; }
+        public string? MartialStautus { get; set; }
 
-        public string? Email { get; set; }     
+
+
+    }
+    public enum Gender
+    {
+            Male,Female
     }
 }
--- CreateWebformusingtaghelpers/Program.cs
+++ CreateWebformusingtaghelpers/Program.cs
@@ -27,5 +27,30 @@ app.MapControllerRoute(
 app.Run();
 
 #region Summary
+/*
+ * asp-for tag helper for input fiels:bind feild with property of model
+ * radio button:
+ * Example Explained
+To style checkboxes, use a wrapper element with class="form-check" to ensure proper margins for labels and checkboxes.
 
+Then, add the .form-check-label class to label elements, and .form-check-input to style checkboxes properly inside the .form-check container.
+
+Use the checked attribute if you want the checkbox to be checked by default.
+form-floating is used for floating label placeholder is required even they not shown
+  *
+ */
+#endregion
+
+#region Model Binding
+/*
+ * It is simple concept that maps the HTTP request to action method parameter
+ * https://localhost:7151/Home/Index/3?name=Hamza for model  binding
+ * How model Binding works:
+ * Order
+ * 1--Form Values:they go in the HTTP request using POST method
+ * 2--Route Values:https://localhost:7151/Home/Index/3
+ * 3--Query string :https://localhost:7151/Home/Index/3?name=Hamza
+ * stored as name-values pair:
+ * 
+ */
 #endregion
