74088a0a1611a616b7513536b360ef659f71ece4
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-07-31 09:52:41 -0700

    Some other model validator

--- CreateWebformusingtaghelpers/Models/Student.cs
+++ CreateWebformusingtaghelpers/Models/Student.cs
@@ -4,19 +4,33 @@ namespace CreateWebformusingtaghelpers.Models
 {
     public class Student
     {
-        [Required(ErrorMessage = "Rollnum is required")]
-        public int? Rollnum{ get; set; }
-        [Required]
-        public string Name { get; set; }
+        [Required(ErrorMessage = "StudentID is required")]
+        public int? StudentID { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string? Name { get; set; }
+
         [Required(ErrorMessage = "Age is required")]
         [Range(20,50)]
         public int?    Age { get; set; }
-       public Gender Gender { get; set; }
+
+       
+        public Gender Gender { get; set; }
         [Required(ErrorMessage = "Email is required")]
-        [EmailAddress]
+        //[RegularExpression("^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$\r\n", ErrorMessage = "invalid email")]
 
         public string? Email { get; set; }
-        
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression("(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$", ErrorMessage = "Must contain  6 charactors UpperCase, LowerCase and Number.")]
+        public string? Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Password must be same")]
+        public string? ConfirmPassword { get; set; }
+
+
+
+
         //public string? MartialStautus { get; set
 
     }
